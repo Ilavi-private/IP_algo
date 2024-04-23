3e6aaa353f0269c94c687310425baae9ef59bc07
Ilavi <83451931+Ilavi-private@users.noreply.github.com>
2024-04-24 02:34:44 +0300

    Updates

---
+++ precache-manifest.4598f79f07eb4b8835e7aa86f99b3e7e.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d0e4bf8f2020345ea201affc731e45",
+    "url": "/VisuAlGo/index.html"
+  },
+  {
+    "revision": "62e4f630bbc6eb36bdfd",
+    "url": "/VisuAlGo/static/css/main.3ec60a3f.chunk.css"
+  },
+  {
+    "revision": "29c5f20174e7f43d63d5",
+    "url": "/VisuAlGo/static/js/2.288586bb.chunk.js"
+  },
+  {
+    "revision": "99562942502e41af72fc73d84afe4332",
+    "url": "/VisuAlGo/static/js/2.288586bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62e4f630bbc6eb36bdfd",
+    "url": "/VisuAlGo/static/js/main.76cab804.chunk.js"
+  },
+  {
+    "revision": "b34359b9359b89ce27c7",
+    "url": "/VisuAlGo/static/js/runtime-main.9c73e72d.js"
+  },
+  {
+    "revision": "fc2cf6f52e5e93d47948562ac011725b",
+    "url": "/VisuAlGo/static/media/AmaticSC-Bold.fc2cf6f5.ttf"
+  },
+  {
+    "revision": "04b6b0803b089211561d2fd5e5d9af80",
+    "url": "/VisuAlGo/static/media/AmaticSC-Regular.04b6b080.ttf"
+  },
+  {
+    "revision": "257898567f4e330e0f5570f63d89cccb",
+    "url": "/VisuAlGo/static/media/Bergamasco Bold Italic.25789856.ttf"
+  },
+  {
+    "revision": "fc825eb08e572f405b2959aaaf6f8d39",
+    "url": "/VisuAlGo/static/media/Bergamasco Bold.fc825eb0.ttf"
+  },
+  {
+    "revision": "26f0cbb198e9ca4a2826a0823b5be6c6",
+    "url": "/VisuAlGo/static/media/Bergamasco Light Italic.26f0cbb1.ttf"
+  },
+  {
+    "revision": "f4a1ef9928c31c22f7620bf6cc1caf5f",
+    "url": "/VisuAlGo/static/media/Bergamasco Light.f4a1ef99.ttf"
+  },
+  {
+    "revision": "7bada6c4bc23c9ac8e071622ac94cd51",
+    "url": "/VisuAlGo/static/media/Bergamasco Regular Italic.7bada6c4.ttf"
+  },
+  {
+    "revision": "166196f36793135c2dba59635cf0081d",
+    "url": "/VisuAlGo/static/media/Bergamasco Regular.166196f3.ttf"
+  },
+  {
+    "revision": "4e08400e296cfa75507b49082b61bb1e",
+    "url": "/VisuAlGo/static/media/Bergamasco Semibold Italic.4e08400e.ttf"
+  },
+  {
+    "revision": "814bc3ae7871300aa014f2bc4d364d33",
+    "url": "/VisuAlGo/static/media/Bergamasco Semibold.814bc3ae.ttf"
+  },
+  {
+    "revision": "a3ae700d7c23679bd20932bf35ffbe5f",
+    "url": "/VisuAlGo/static/media/Journalism.a3ae700d.ttf"
+  },
+  {
+    "revision": "8532b8d86af0f6139f6a0b8e6d2af606",
+    "url": "/VisuAlGo/static/media/_hover=False, module=linkedList.8532b8d8.svg"
+  },
+  {
+    "revision": "aaaf3f0072fe7a1c5cc8b0645700c3fe",
+    "url": "/VisuAlGo/static/media/_hover=False, module=queue.aaaf3f00.svg"
+  },
+  {
+    "revision": "244026cd9f57fd3bb801c544b5d76f70",
+    "url": "/VisuAlGo/static/media/_hover=False, module=sorting.244026cd.svg"
+  },
+  {
+    "revision": "2458efc0aa50961abefe737d0f3c2b8c",
+    "url": "/VisuAlGo/static/media/_hover=False, module=stack.2458efc0.svg"
+  },
+  {
+    "revision": "03c6b341e5a933c066a84301cacfd338",
+    "url": "/VisuAlGo/static/media/_hover=True, module=linkedList.03c6b341.svg"
+  },
+  {
+    "revision": "9d51a423838547be8c57e55534d68526",
+    "url": "/VisuAlGo/static/media/_hover=True, module=queue.9d51a423.svg"
+  },
+  {
+    "revision": "73f38d1fceee73af339f518665317e2e",
+    "url": "/VisuAlGo/static/media/_hover=True, module=sorting.73f38d1f.svg"
+  },
+  {
+    "revision": "3e9bb2ba0da3700a2008d95713b0df21",
+    "url": "/VisuAlGo/static/media/_hover=True, module=stack.3e9bb2ba.svg"
+  },
+  {
+    "revision": "0b04fecee65e3980cb6bd91bfbf2e233",
+    "url": "/VisuAlGo/static/media/loader.0b04fece.svg"
+  }
+]);
